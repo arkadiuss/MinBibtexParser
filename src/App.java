68af1b56db3c616124c0a95a7867cc570052c2d2
arkadius <arkadius.kraus@gmail.com>
2018-12-03 23:12:05 +0100

    add common cli - initial

--- src/App.java
+++ src/App.java
@@ -1,12 +1,12 @@
 import exception.ParseException;
 import filereader.BibtexFileReader;
 import model.Entry;
+import org.apache.commons.cli.*;
 import parser.MinimalBibtexParser;
 import repository.EntryRepository;
 import repository.IEntryRepository;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 
 public class App {
@@ -15,6 +15,17 @@ public class App {
     public static void main(String[] args){
         BibtexFileReader fileReader = new BibtexFileReader();
         IEntryRepository entryRepository = new EntryRepository();
+
+
+        CommandLineParser parser = new DefaultParser();
+        Options options = createCommandLineOptions();
+        HelpFormatter helpFormatter = new HelpFormatter();
+        helpFormatter.printHelp("syntax", options);
+        try {
+            CommandLine commandLine = parser.parse(options, args);
+        } catch (org.apache.commons.cli.ParseException e) {
+            e.printStackTrace();
+        }
         try {
             String bibtexString = fileReader.read(args[0]);
             MinimalBibtexParser mbp = new MinimalBibtexParser();
@@ -25,5 +36,23 @@ public class App {
         } catch (IOException | ParseException e) {
             e.printStackTrace();
         }
+    }
+
+    private static Options createCommandLineOptions(){
+        Option filename;
+        Option types = Option.builder()
+                .argName("categories")
+                .longOpt("cat")
+                .desc("Categories")
+                .build();
+        Option names = Option.builder()
+                .argName("anames")
+                .longOpt("anam")
+                .desc("Author names")
+                .build();
+        Options options = new Options();
+        options.addOption(types);
+        options.addOption(names);
+        return options;
     }
 }
