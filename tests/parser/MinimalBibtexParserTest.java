01bb683b718516db9264ce6014a881377d2e0996
arkadius <arkadius.kraus@gmail.com>
2018-12-03 18:02:55 +0100

    string variables test

--- tests/parser/MinimalBibtexParserTest.java
+++ tests/parser/MinimalBibtexParserTest.java
@@ -6,38 +6,56 @@ import model.Field;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MinimalBibtexParserTest {
+    private IBibtexParser parser = new MinimalBibtexParser();
+    private String basicAuthor = "Good basicAuthor";
+    private String basicTitle = "Good book";
+    private String basicPublisher = "Good publisher";
+    private String basicYear = "1900";
 
     @Test
     void shouldParseSimpleEntry() throws ParseException {
-        IBibtexParser parser = new MinimalBibtexParser();
-        String author = "Good author";
-        String title = "Good book";
-        String publisher = "Good publisher";
-        String year = "1900";
         String entry = "@BOOK{\n" +
-                "author = " + wrapWithQuote(author) + "\n," +
-                "title = "+wrapWithQuote(title)+"\n,"+
-                "publisher = "+wrapWithQuote(publisher)+"\n,"+
-                "year="+wrapWithQuote(year)+"\n"+
+                "author = " + wrapWithQuote(basicAuthor) + "\n," +
+                "title = "+wrapWithQuote(basicTitle)+"\n,"+
+                "publisher = "+wrapWithQuote(basicPublisher)+"\n,"+
+                "year="+wrapWithQuote(basicYear)+"\n"+
                 "}";
         List<Entry> entries = parser.parse(entry);
         assertEquals(1,entries.size());
         Entry e = entries.get(0);
-        String[] entriesFields =
-                Arrays.stream(e.getFields())
-                .map(Field::getValue)
-                .filter(Objects::nonNull)
-                .toArray(String[]::new);
         assertArrayEquals(
-                new String[]{author, title, publisher,year},
-                entriesFields);
+                new String[]{basicAuthor, basicTitle, basicPublisher, basicYear},
+                getEntriesList(e));
+    }
+
+    @Test
+    void shouldParseEntryWithStringVariable(){
+        String titleWithVariable = "\"Title\" # variable # \"end\""
+        String entry = "@BOOK{\n" +
+                "author = " + titleWithVariable + "\n," +
+                "title = "+wrapWithQuote(basicTitle)+"\n,"+
+                "publisher = "+wrapWithQuote(basicPublisher)+"\n,"+
+                "year="+wrapWithQuote(basicYear)+"\n"+
+                "}";
+        List<Entry> entries = parser.parse(entry);
+        assertEquals(1,entries.size());
+        Entry e = entries.get(0);
+        assertArrayEquals(
+                new String[]{basicAuthor, basicTitle, basicPublisher, basicYear},
+                getEntriesList(e));
+    }
+
+    private String[] getEntriesList(Entry e){
+        return Arrays.stream(e.getFields())
+                        .map(Field::getValue)
+                        .filter(Objects::nonNull)
+                        .toArray(String[]::new);
     }
 
     private String wrapWithQuote(String s){
