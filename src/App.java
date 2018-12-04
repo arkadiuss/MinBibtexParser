eb9dd07abc61f990e3ba4419f2fefcbd5ba73ab3
arkadius <arkadius.kraus@gmail.com>
2018-12-04 01:08:08 +0100

    basic program arguments processing

--- src/App.java
+++ src/App.java
@@ -11,48 +11,63 @@ import java.util.List;
 
 public class App {
 
-
     public static void main(String[] args){
         BibtexFileReader fileReader = new BibtexFileReader();
         IEntryRepository entryRepository = new EntryRepository();
-
-
         CommandLineParser parser = new DefaultParser();
-        Options options = createCommandLineOptions();
         HelpFormatter helpFormatter = new HelpFormatter();
-        helpFormatter.printHelp("syntax", options);
+        Options options = createCommandLineOptions();
+        helpFormatter.printHelp("parser <FILE> [OPTIONS]", options);
+        CommandLine commandLine = null;
         try {
-            CommandLine commandLine = parser.parse(options, args);
+            commandLine = parser.parse(options, args);
         } catch (org.apache.commons.cli.ParseException e) {
             e.printStackTrace();
+            return;
         }
-        try {
-            String bibtexString = fileReader.read(args[0]);
-            MinimalBibtexParser mbp = new MinimalBibtexParser();
-            List<Entry> parsed = mbp.parse(bibtexString);
-            entryRepository.addAll(parsed);
-            entryRepository.getAllByAuthorName("Les")
-                    .forEach(System.out::println);
-        } catch (IOException | ParseException e) {
-            e.printStackTrace();
+        if(commandLine.hasOption("help")){
+            helpFormatter.printHelp("parser <FILE> [OPTIONS]", options);
+        }else{
+            try {
+                String bibtexString = fileReader.read(commandLine.getArgs()[0]);
+                MinimalBibtexParser mbp = new MinimalBibtexParser();
+                List<Entry> parsed = mbp.parse(bibtexString);
+                entryRepository.addAll(parsed);
+            } catch (IOException | ParseException e) {
+                e.printStackTrace();
+            }
+            List<Entry> entriesFromRepo;
+            if(commandLine.hasOption("categories")){
+                entriesFromRepo = entryRepository.getAll();
+            }else if(commandLine.hasOption("anames")){
+                entriesFromRepo = entryRepository.getAll();
+            }else{
+                entriesFromRepo = entryRepository.getAll();
+            }
+            entriesFromRepo.forEach(System.out::println);
         }
     }
 
     private static Options createCommandLineOptions(){
-        Option filename;
+        Option help = Option.builder()
+                .argName("help")
+                .longOpt("help")
+                .desc("Show this help")
+                .build();
         Option types = Option.builder()
                 .argName("categories")
                 .longOpt("cat")
-                .desc("Categories")
+                .desc("Filter selected records from file by category")
                 .build();
         Option names = Option.builder()
-                .argName("anames")
-                .longOpt("anam")
-                .desc("Author names")
+                .argName("authors' names")
+                .longOpt("anames")
+                .desc("Filter selected records from file by authors' surnames")
                 .build();
         Options options = new Options();
         options.addOption(types);
         options.addOption(names);
+        options.addOption(help);
         return options;
     }
 }
