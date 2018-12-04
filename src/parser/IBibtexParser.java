61900c281da1334adcbdc07114edc2737301e455
arkadius <arkadius.kraus@gmail.com>
2018-12-04 20:01:58 +0100

    javadoc for parser

--- src/parser/IBibtexParser.java
+++ src/parser/IBibtexParser.java
@@ -5,6 +5,22 @@ import model.Entry;
 
 import java.util.List;
 
+/**
+ * Bibtex parser which enables to
+ * validate and parse given bibtex
+ * to object representation
+ */
 public interface IBibtexParser {
+    /**
+     * Method that parse given string, which
+     * contains bibtex, to Entry, which is
+     * an object representation of given
+     * records. It also validate if given
+     * string match bibtex format.
+     *
+     * @param bibtex String that contains content of bibtex file
+     * @return List of entries from bibtex string
+     * @throws ParseException when format of file is incorrect
+     */
     List<Entry> parse(String bibtex) throws ParseException;
 }
--- src/parser/MinimalBibtexParser.java
+++ src/parser/MinimalBibtexParser.java
@@ -3,12 +3,26 @@ package parser;
 import exception.ParseException;
 import model.Entry;
 import model.EntryBuilder;
-import model.EntryType;
 
 import java.util.*;
 
+/**
+ * Implementation of bibtex parser, which
+ * allows user to parse simplified bibtex
+ * Simplified bibtex format is described here
+ */
 public class MinimalBibtexParser implements IBibtexParser {
 
+    /**
+     * {@inheritDoc}
+     *
+     * This implementation firstly split
+     * all the objects by '@' sign what
+     * enables to keep all entries seperately
+     * and then iterate over all records to process them.
+     * It also declared a map to keep bibtex string
+     * variables.
+     */
     @Override
     public List<Entry> parse(String bibtex) throws ParseException {
         String[] entries = bibtex.split("@");
@@ -22,6 +36,17 @@ public class MinimalBibtexParser implements IBibtexParser {
         return parsedEntries;
     }
 
+    /**
+     * Method that looks for first '{' sign and last '}'
+     * to determine range of the entry and then
+     * it takes type from the entry and proccess
+     * it as entry or a variable
+     *
+     * @param entry One of the entries without @ sign
+     * @param variables Maps that keep bibtex variables
+     * @return Processed entry or null if record is a string variable
+     * @throws ParseException when brackets are not found
+     */
     private Entry processObject(String entry, Map<String, String> variables)
             throws ParseException {
         int typeLen = entry.indexOf('{');
@@ -40,6 +65,18 @@ public class MinimalBibtexParser implements IBibtexParser {
         }
     }
 
+    /**
+     * Method that process entry without surrounding
+     * brackets. It firstly split record by , sign
+     * and then divide every field to name and value
+     * by = sign, which are inserted into EntryBuilder
+     *
+     * @param fields Not splitted entry without surrounding brackets
+     * @param type Type of the entry
+     * @param variables Map of string variables
+     * @return Builded entry with values
+     * @throws ParseException
+     */
     private Entry processEntry(String fields, String type, Map<String, String> variables)
             throws ParseException {
         EntryBuilder builder = new EntryBuilder();
@@ -56,6 +93,17 @@ public class MinimalBibtexParser implements IBibtexParser {
         return builder.build();
     }
 
+    /**
+     * One value of field can contain strings and
+     * variables. This method change every variable
+     * to its value and then connect everything into
+     * one value
+     *
+     * @param fieldValue Value of the field with variables
+     * @param variables Map of variables
+     * @return Value with inserted values of variables
+     * @throws ParseException when variable is not found
+     */
     private String getValueOfField(String fieldValue, Map<String, String> variables) throws ParseException {
         String[] parts =
                 Arrays.stream(fieldValue.split("#"))
@@ -71,13 +119,16 @@ public class MinimalBibtexParser implements IBibtexParser {
                 parts[i] = fromVar;
             }
         }
-        return reduce(parts);
-    }
-
-    private String reduce(String[] parts){
         return Arrays.stream(parts).reduce(String::concat).orElse("");
     }
 
+    /**
+     * Method that add a variable to map of variables
+     * which enables to use them later
+     *
+     * @param fields Content of record without surrounding brackets
+     * @param variables Map of variables to add new variable
+     */
     private void processVariable(String fields,  Map<String, String> variables){
         String[] nameAndValue = fields.split("=");
         variables.put(
